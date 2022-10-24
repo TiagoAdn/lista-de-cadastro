b84abe6a2c9182cacfa04d73e527ac204076bd86
Tiago Silva <tiago.silva.adn35@gmail.com>
2022-10-24 09:20:29 -0300

    lista de cadastro

---
+++ lista de cadastro.js
@@ -0,0 +1,13 @@
+// se o participante for maior de 18 anos,permitir o cadastro;senão,alertar que o cadastro não e permitido pela idade.
+// listar participantes e palestrantes por evento.
+// emquanto a quantidade de participantes for inferior a 100 , permitir cadastro; senão ,alerta que o cadastro não será permitido por ter excedido o limite.
+var idade = ("18")
+if (idade <18)
+  console.log("idade nao permitida")
+else
+    console.log("idade permitida")
+let listadeparticipantes = 100
+if (listadeparticipantes <100)
+   console.log("participante cadastrado")
+else
+   console.log("excedido o limite de participantes")
